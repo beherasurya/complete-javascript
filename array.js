7a46c0b3fbe90d143279830ef3714cf579ec1ded
beherasurya <beherasuryakanta18@gmail.com>
2023-01-22 10:08:45 +0530

    first file in javascript

---
+++ array.js
@@ -0,0 +1,8 @@
+
+let firstString="India is the best country in the world";
+
+let storeString=firstString.split(" ");
+
+console.log(storeString[2]);
+storeString.reverse();
+console.log(storeString.toString());
