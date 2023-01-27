ba0393648a5dbceac6d3605198ecd28555ca5ba9
beherasurya <beherasuryakanta18@gmail.com>
2023-01-27 19:57:13 +0530

    coding challenges updated

--- array.js
+++ array.js
@@ -1,8 +1,10 @@
 
-let firstString="India is the best country in the world";
+// let firstString="India is the best country in the world";
 
-let storeString=firstString.split(" ");
+// let storeString=firstString.split(" ");
 
-console.log(storeString[2]);
-storeString.reverse();
-console.log(storeString.toString());
+// console.log(storeString[2]);
+// storeString.reverse();
+// console.log(storeString.toString());
+
+console.log(Array.from([10,20,30]));
