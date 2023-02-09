4872b1828607a87c1638176b21ffcb4b0d3555aa
beherasurya <beherasuryakanta18@gmail.com>
2023-02-09 19:02:02 +0530

    clousers updated

---
+++ clousers-assignment.js
@@ -0,0 +1,18 @@
+// let holdElement;
+(function(){
+    const header= document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.addEventListener("click" , function(){
+        header.style.color = "blue" ;
+    });
+   
+})
+();
+
+
+// let callBackFunction= ();
+
+// document.body.addEventListener("click" ,());
+
+
